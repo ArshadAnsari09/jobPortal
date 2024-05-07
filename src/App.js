c69311cde87babf391c6e81437c042512850b9b2
Arshad Ansari <arshad.ansari.e21@nsut.ac.in>
2024-05-07 22:38:04 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+// import JobDetails from "./components/JobDetails";
+// import JobList from "./components/JobList";
+
+function App() {
+  return (
+    <div>
+      {/* <JobList /> */}
+      <h1>Job Portal</h1>
+    </div>
+  );
+}
+
+export default App;
