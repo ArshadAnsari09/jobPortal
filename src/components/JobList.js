b17b600631f7d748192d3265d1ab6af67c5cef66
Arshad Ansari <arshad.ansari.e21@nsut.ac.in>
2024-05-09 00:14:00 +0530

    fixing filter issue and adding css

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -7,6 +7,7 @@ import {
   InputAdornment,
   IconButton,
   Grid,
+  Button,
   CircularProgress,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
@@ -15,26 +16,33 @@ import InfiniteScroll from "react-infinite-scroll-component";
 const JobList = () => {
   const dispatch = useDispatch();
   const jobs = useSelector(selectAllJobs);
-  const [filters, setFilters] = useState({
-    companyName: "",
-    location: "",
-    minExperience: "",
-    remote: false,
-    techStack: "",
-    role: "",
-    salary: "",
-  });
+  const [filteredJobs, setFilteredJobs] = useState([]);
 
   useEffect(() => {
     dispatch(fetchJobsAsync({ limit: 10, offset: 0 })); // Initial fetch
   }, [dispatch]);
-  console.log("Jobs", jobs);
+
+  useEffect(() => {
+    setFilteredJobs(jobs); // Update filteredJobs when jobs are fetched or changed
+  }, [jobs]);
+
+  const [filters, setFilters] = useState({
+    companyName: "",
+    location: "",
+    minExperience: "",
+    maxExperience: "",
+    remote: false,
+    techStack: "",
+    role: "",
+    minSalary: "",
+    maxSalary: "",
+  });
 
   const applyFilters = () => {
-    let filteredJobs = [...jobs];
+    let result = jobs;
 
     if (filters.companyName) {
-      filteredJobs = filteredJobs.filter((job) =>
+      result = result.filter((job) =>
         job.companyName
           .toLowerCase()
           .includes(filters.companyName.toLowerCase())
@@ -42,49 +50,68 @@ const JobList = () => {
     }
 
     if (filters.location) {
-      filteredJobs = filteredJobs.filter((job) =>
+      result = result.filter((job) =>
         job.location.toLowerCase().includes(filters.location.toLowerCase())
       );
     }
 
     if (filters.role) {
-      filteredJobs = filteredJobs.filter((job) =>
+      result = result.filter((job) =>
         job.jobRole.toLowerCase().includes(filters.role.toLowerCase())
       );
     }
 
-    if (filters.minExperience >= 0) {
-      filteredJobs = filteredJobs.filter(
-        (job) => job.minExp && job.minExp >= filters.minExperience
-      );
+    const minExp = parseInt(filters.minExperience);
+    const maxExp = parseInt(filters.maxExperience);
+    const minSalary = parseInt(filters.minSalary);
+    const maxSalary = parseInt(filters.maxSalary);
+
+    if (minExp) {
+      result = result.filter((job) => job.minExp >= minExp);
     }
 
-    if (filters.salary > 0) {
-      filteredJobs = filteredJobs.filter(
-        (job) => job.minJdSalary && job.minJdSalary >= filters.salary
-      );
+    if (maxExp) {
+      result = result.filter((job) => job.maxExp <= maxExp);
     }
-    return filteredJobs;
+
+    if (minSalary) {
+      result = result.filter((job) => job.minJdSalary >= minSalary);
+    }
+
+    if (maxSalary) {
+      result = result.filter((job) => job.maxJdSalary <= maxSalary);
+    }
+
+    setFilteredJobs(result);
   };
 
   const handleFilterChange = (field, value) => {
-    setFilters({
-      ...filters,
+    setFilters((prev) => ({
+      ...prev,
       [field]: value,
-    });
+    }));
   };
 
   const loadMoreJobs = () => {
-    // Dispatch an action to fetch more jobs with an increased offset
     dispatch(fetchJobsAsync({ limit: 10, offset: jobs.length }));
   };
 
-  let filteredJobs = applyFilters();
+  // let filteredJobs = applyFilters();
 
   return (
     <div>
-      <Grid container spacing={2} alignItems="center" style={{ marginTop: 30 }}>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+      <Grid
+        container
+        spacing={1}
+        alignItems="center"
+        style={{
+          marginTop: 30,
+          // border: "1px solid black",
+          width: "80vw",
+          marginLeft: 100,
+        }}
+      >
+        <Grid item xs={6} sm={4} md={3}>
           <TextField
             label="Company Name"
             fullWidth
@@ -102,7 +129,7 @@ const JobList = () => {
             }}
           />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={6} sm={4} md={3}>
           <TextField
             label="Location"
             fullWidth
@@ -120,9 +147,9 @@ const JobList = () => {
             }}
           />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={6} sm={4} md={3}>
           <TextField
-            label="Experience"
+            label="Min Experience"
             fullWidth
             size="small"
             type="number"
@@ -141,13 +168,16 @@ const JobList = () => {
             }}
           />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={6} sm={4} md={3}>
           <TextField
-            label="Salary"
+            label="Max Experience" // New field for max experience
             fullWidth
             size="small"
-            value={filters.salary}
-            onChange={(e) => handleFilterChange("salary", e.target.value)}
+            type="number"
+            value={filters.maxExperience}
+            onChange={(e) =>
+              handleFilterChange("maxExperience", e.target.value)
+            }
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -159,7 +189,45 @@ const JobList = () => {
             }}
           />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={6} sm={4} md={3}>
+          <TextField
+            label="Min Salary"
+            fullWidth
+            size="small"
+            type="number"
+            value={filters.minJdSalary}
+            onChange={(e) => handleFilterChange("minJdSalary", e.target.value)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IconButton size="small">
+                    <SearchIcon fontSize="small" />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3}>
+          <TextField
+            label="Max Salary"
+            fullWidth
+            size="small"
+            type="number"
+            value={filters.maxJdSalary}
+            onChange={(e) => handleFilterChange("maxJdSalary", e.target.value)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IconButton size="small">
+                    <SearchIcon fontSize="small" />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3}>
           <TextField
             label="Role"
             fullWidth
@@ -177,9 +245,19 @@ const JobList = () => {
             }}
           />
         </Grid>
+        <Grid item xs={6} sm={4} md={3}>
+          <Button
+            variant="contained"
+            color="secondary"
+            fullWidth
+            onClick={applyFilters}
+          >
+            Apply Filters
+          </Button>
+        </Grid>
       </Grid>
       <InfiniteScroll
-        style={{ overflowY: "hidden" }}
+        style={{ overflowY: "hidden", marginTop: 40 }}
         dataLength={filteredJobs.length}
         next={loadMoreJobs}
         hasMore={jobs.length % 10 === 0}
